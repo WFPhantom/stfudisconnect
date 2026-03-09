95b2da06d3bf77b2e12393eee41f789e4af73ea3
WFPhantom <wfphantom5@gmail.com>
2026-03-09 13:55:12 +0200

    initial commit

---
+++ src/main/java/com/wfphantom/stfudisconnect/STFUDisconnect.java
@@ -0,0 +1,8 @@
+package com.wfphantom.stfudisconnect;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(STFUDisconnect.MODID)
+public class STFUDisconnect {
+    public static final String MODID = "stfudisconnect";
+}
---
+++ src/main/java/com/wfphantom/stfudisconnect/mixin/ConnectionMixin.java
@@ -0,0 +1,30 @@
+package com.wfphantom.stfudisconnect.mixin;
+
+import net.minecraft.network.Connection;
+import net.minecraft.network.ConnectionProtocol;
+import net.minecraft.network.PacketListener;
+import net.minecraft.network.protocol.PacketFlow;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Connection.class)
+public abstract class ConnectionMixin {
+
+    @Shadow
+    private PacketListener packetListener;
+
+    @Shadow
+    public abstract PacketFlow getSending();
+
+    @Redirect(method = "exceptionCaught", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Connection;getSending()Lnet/minecraft/network/protocol/PacketFlow;"))
+    private PacketFlow shutthefuckup(Connection instance) {
+        PacketListener listener = this.packetListener;
+        if (listener != null) {
+            ConnectionProtocol protocol = listener.protocol();
+            if (protocol == ConnectionProtocol.STATUS || protocol == ConnectionProtocol.HANDSHAKING) return PacketFlow.SERVERBOUND;
+        }
+        return this.getSending();
+    }
+}
