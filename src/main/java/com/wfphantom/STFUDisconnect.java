2178fc5665b18d45d3fdca9a38f5c1f2d310876f
WFPhantom <wfphantom5@gmail.com>
2026-06-17 10:30:36 +0300

    port to 26.2

--- src/main/java/com/wfphantom/STFUDisconnect.java
+++ src/main/java/com/wfphantom/STFUDisconnect.java
@@ -3,7 +3,6 @@ package com.wfphantom;
 import net.fabricmc.api.ModInitializer;
 
 public class STFUDisconnect implements ModInitializer {
-	public static final String MOD_ID = "stfudisconnect";
 	@Override
 	public void onInitialize() {}
 }
